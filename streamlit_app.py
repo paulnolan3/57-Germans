5fe3a15ab2fb06fd9ab6b1d390bbe3d9ab45da3d
paulnolan3 <144174178+paulnolan3@users.noreply.github.com>
2024-05-20 14:45:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Sample data
+data = {
+    'Day': [1, 2, 3, 4, 5, 6, 7],
+    'Germans': [2, 3, 5, 7, 8, 6, 7]
+}
+
+df = pd.DataFrame(data)
+
+st.title('Number of Germans Living in Our Home Over Time')
+st.subheader('Interactive Chart')
+
+st.line_chart(df.set_index('Day'))
+
+# Optional: Adding more interactive features
+st.write('Data Table')
+st.dataframe(df)
+
+st.write('Statistics')
+st.write(df.describe())
